1562fa0c2f3e4ba63b68a1b4155af1c8e25f83ef
Dima Portenko <troublesohard@gmail.com>
2023-07-12 11:21:58 +0300

    test implementation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,57 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, {useCallback} from 'react';
+import {
+  FlatList,
+  ListRenderItemInfo,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+import Animated, {
+  Easing,
+  Layout,
+  ZoomIn,
+  ZoomOut,
+} from 'react-native-reanimated';
 
-const App = () => {
+import {DataItemType, getData} from './src/data';
+import {ListItem} from './src/ListItem';
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
+export default function App() {
+  const data = getData();
+  console.log(data);
+
+  const renderItem = ({item}: ListRenderItemInfo<DataItemType>) => {
+    return <ListItem item={item} />;
+  };
+
+  const renderCell = useCallback((props: any) => {
+    return (
+      <Animated.View
+        {...props}
+        // layout={Layout.springify()}
+        layout={Layout.easing(Easing.ease)}
+        // entering={ZoomIn}
+        exiting={ZoomOut}
+      />
+    );
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello</Text>
+      <AnimatedFlatList
+        data={data}
+        keyExtractor={(item: DataItemType, index) => `${item.title}${index}`}
+        renderItem={renderItem}
+        CellRendererComponent={renderCell}
+      />
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container: {flex: 1, alignSelf: 'center', justifyContent: 'center'},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
 });
-
-export default App;
